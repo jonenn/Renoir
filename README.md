8117f36eacd4024e50157950d898bbcc6e21836f
Juan J <juanmagri@outlook.es>
2020-11-12 00:34:01 -0500

    Home images setup.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Michelangelo
-In this project I am going to be working on a first mobile module, in which I will be reinforcing knowledge of CSS Grid. It is a gallery based on the masterpieces of Michelangelo in essence.
+# Renoir
+In this project I am going to be working on a first-mobile module, in which I will be reinforcing knowledge of CSS Grid. It is a gallery based on the masterpieces of Renoir in essence.
+This project was intended to be a Michelangelo paintings based website, but as the development advanced the approach corresponding with design in relation with his pieces also changed.
--- global.js
+++ global.js
@@ -1,14 +1,20 @@
-var search = document.querySelector(".search");
-var searching = document.querySelector(".searching");
 var content = document.getElementById("contentWrapper");
-search.addEventListener("click", activate);
+var header = document.getElementById("header");
+var searching = document.getElementById("searching");
+var search_que = document.querySelector(".search");
+var searching_que = document.querySelector(".searching");
+search_que.addEventListener("click", activate);
 
 function activate () {
-   if (searching.classList.contains("active")) {
-      searching.classList.remove("active");
-      content.style.top = "-42px";
+   if (searching_que.classList.contains("active")) {
+      searching_que.classList.remove("active");
+      content.style.top="-42px";
+      header.style.boxShadow="0 1px 9px rgba(35, 0, 19 , .3)";
+      searching.style.boxShadow="none";
    } else {
-      searching.classList.add("active");
-      content.style.top = "0";
+      searching_que.classList.add("active");
+      content.style.top="0";
+      header.style.boxShadow="none";
+      searching.style.boxShadow="0 1px 9px rgba(35, 0, 19 , .3)";
    }
 }
