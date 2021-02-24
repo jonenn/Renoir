875d5767eade16be2993e7eedf67d66d0cb3bea0
Juan J <juanmagri@outlook.es>
2021-02-24 11:48:44 -0500

    See more and see less fully working on about.html

--- about.js
+++ about.js
@@ -1,3 +1,7 @@
+let extend = document.getElementById("extendIt");
+let see = document.getElementById("seeIt");
+let seeContent = document.getElementById("see-link");
+
 window.addEventListener("scroll", scrollFunc);
 
 function scrollFunc() {
@@ -5,3 +9,14 @@ function scrollFunc() {
    let banner = document.getElementsByClassName("banner-bg");
    banner[0].style.transform = `scale( ${windowScroll / 1e3})`;
 }
+
+const seeMore = () => {
+   let extendStyle = extend.style;
+   extendStyle.height === ""
+      ? (extendStyle.height = "auto",
+      seeContent.innerHTML = "see less<img src='images/see-less.png'>")
+      : (extendStyle.height = "",
+      seeContent.innerHTML = "see more<img src='images/see-more.png'>");
+}
+
+see.addEventListener("click", seeMore);
